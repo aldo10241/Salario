458d2a3bbede1570967d9140c417e8149c1c6bb6
mraugm <prrotruste@gmail.com>
2022-03-12 15:55:11 -0600

    Se agregan primeros archivos

---
+++ analisis.js
@@ -0,0 +1,9 @@
+const var1 = document.getElementById("salario");
+const salario = var1.value;
+
+const var2 = document.getElementById("dobles");
+const dobles = var2.value;
+
+function calcularHoras(){
+    const total = salario 
+}
