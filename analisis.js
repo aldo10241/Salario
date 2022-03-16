7d900815f24484f97bdbd72430fb8ab41bc79e30
mraugm <prrotruste@gmail.com>
2022-03-16 08:54:31 -0600

    Version 1

--- analisis.js
+++
@@ -1,9 +0,0 @@
-const var1 = document.getElementById("salario");
-const salario = var1.value;
-
-const var2 = document.getElementById("dobles");
-const dobles = var2.value;
-
-function calcularHoras(){
-    const total = salario 
-}
